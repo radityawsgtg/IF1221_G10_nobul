d01bf7e18efc16d697bd2c81e0529a63af11ca55
Raditya Sastaka <radityaws79@gmail.com>
2026-05-29 00:05:17 +0700

    fix: conflic fix

--- src/setup.pl
+++ src/setup.pl
@@ -22,6 +22,10 @@ startGame :-
         random_permutation(ListPemain, UrutanAcak),
         asserta(urutan_pemain(UrutanAcak)),
 
+        semua_kartu(FullDeck),
+        random_permutation(FullDeck, ShuffledDeck),
+        asserta(tumpukan_dek(ShuffledDeck)),
+
         bagi_kartu_awal(UrutanAcak),
         inisiasi_discard,
         UrutanAcak = [PemainPertama|_],
@@ -50,12 +54,12 @@ inisiasi_pemain(N, Terdaftar, Hasil) :-
     (\+ cek_huruf_besar(Nama) ->
         write('Nama harus diawali dengan huruf besar! Coba lagi.'), nl,
         inisiasi_pemain(N, Terdaftar, Hasil)
-    ; member(Nama, Terdaftar) ->
+    ; is_element(Nama, Terdaftar) ->
         write('Nama sudah digunakan. Masukkan nama lain: '), nl,
         inisiasi_pemain(N, Terdaftar, Hasil)
     ;
         N1 is N-1,
-        append(Terdaftar, [Nama], TerdaftarBaru),
+        append_list(Terdaftar, [Nama], TerdaftarBaru),
         inisiasi_pemain(N1, TerdaftarBaru, Hasil)
     ).
 
@@ -120,21 +124,3 @@ bagi_n_kartu(N, [Kartu|T]) :-
     kartu_acak(Kartu),
     N1 is N-1,
     bagi_n_kartu(N1, T).
-
-inisiasi_discard :-
-    findall(kartu(W, A), (warna(W), jenis_angka(A)), ListAngka),
-    random_member(KartuAwal, ListAngka),
-    asserta(discard_top(KartuAwal)).
-
-random_member(X, L) :-
-    length(L, Len),
-    random(0, Len, Index),
-    nth0(Index, L, X).
-
-random_permutation(L, R) :-
-    findall(Rand-X, (member(X, L), random(Rand)), Pairs),
-    keysort(Pairs, Sorted),
-    findall(X, member(_-X, Sorted), R).
-
-nth0(0, [H|_], H) :- !.
-nth0(N, [_|T], H) :- N > 0, N1 is N-1, nth0(N1, T, H).
